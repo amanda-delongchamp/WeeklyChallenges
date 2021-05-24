2c1b7d929846cd282b855cd3fd6bf30a1f431648
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-05-23 18:14:30 -0700

    Challenge 03 Complete

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace ChallengesWithTestsMark8
@@ -7,47 +8,140 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+
+                
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+
+            var sum = numbers.Sum();
+            return (sum % 2 != 0);
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool isLower = false;
+            bool isUpper = false;
+            bool isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true;
+                }
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+
+              
+
+            }
+            if (isLower == true && isUpper == true && isNumber == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            var letters = val.ToCharArray();
+
+            char firstLetter = letters[0];
+
+            return firstLetter;
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            var letters = val.ToCharArray();
+
+            char lastLetter = letters[val.Length -1];
+
+            return lastLetter;
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            
+                return dividend / divisor;
+            
+            
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            int last = 0;
+            int first = 0;
+
+            /*for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    first = nums[i];
+                }
+                if (nums[i] == nums.Length - 1)
+                {
+                    last = nums[i];
+                }
+            }*/
+
+            last = nums[nums.Length - 1];
+            first = nums[0];
+
+            return last - first;
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var odds = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+
+            return odds.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
